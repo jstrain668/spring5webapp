879cc860f584922d2de38e8f921623f7fb1f0d3f
Developer <jjstrain68@gmail.com>
2021-02-05 22:22:01 +0000

    Created domain object Publisher along with repository and updated Boostrap class to save one publisher

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRespository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -14,6 +16,7 @@ public class BootStrapData implements CommandLineRunner {
 
     private final AuthorRespository authorRespository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -24,9 +27,13 @@ public class BootStrapData implements CommandLineRunner {
         Book ddd = new Book("Domain Driven Design","123123");
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
+        Publisher wrox = new Publisher("Wrox","17 Easy Street","New York","NY","ZX11");
 
         authorRespository.save(eric);
         bookRepository.save(ddd);
+        publisherRepository.save(wrox);
+
+        System.out.println("Number of publishers: "+publisherRepository.count());
 
         Author rod = new Author("Rod", "Johnson");
         Book noEJB = new Book("J2EE development without EJB","3933933930");
