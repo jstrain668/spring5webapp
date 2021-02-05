2437dfb20adc40b53bb8f9246007581784ba4046
Developer <jjstrain68@gmail.com>
2021-02-05 19:54:29 +0000

    Created domain objects, book and authory along with respective repository objects and Boostrap class to save two books and author objects Added junit5 dependency to pom. Removed junit4 from Spring5webappApplicationTests

---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -0,0 +1,42 @@
+package guru.springframework.spring5webapp.bootstrap;
+
+import guru.springframework.spring5webapp.domain.Author;
+import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.repositories.AuthorRespository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class BootStrapData implements CommandLineRunner {
+
+    private final AuthorRespository authorRespository;
+    private final BookRepository bookRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        System.out.println("Started in Bootstrap");
+
+        Author eric = new Author("Eric","Evans");
+        Book ddd = new Book("Domain Driven Design","123123");
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        authorRespository.save(eric);
+        bookRepository.save(ddd);
+
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE development without EJB","3933933930");
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+
+        authorRespository.save(rod);
+        bookRepository.save(noEJB);
+
+        System.out.println("Number of books: "+bookRepository.count());
+
+    }
+}
