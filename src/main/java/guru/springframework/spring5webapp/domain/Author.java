ee00d9c4aeb9071c3f941ed976cb86dd3c501122
Developer <jjstrain68@gmail.com>
2021-02-06 00:52:09 +0000

    Added Thymeleaf (list.html), removed books and authors form Book toString and similarily for Author. Updated BookController and pom.xml to bring in thymeleaf

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -36,7 +36,6 @@ public class Author {
                 "id=" + id +
                 ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
-                ", books=" + books +
                 '}';
     }
 
--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -41,7 +41,6 @@ public class Book {
                 "id=" + id +
                 ", title='" + title + '\'' +
                 ", isbn='" + isbn + '\'' +
-                ", authors=" + authors +
                 '}';
     }
 
